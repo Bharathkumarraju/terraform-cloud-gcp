7e9ed65e8c85ec0d687649913f6b3234a137ca4a
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2021-01-22 14:37:48 +0800

    update creds file

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   region = "us-east4"
   zone = "us-east4-a"
-  credentials = file("/Users/bharathkumarraju/Documents/work/2021/bharaths-google-credentials.json")
+#  credentials = file("/Users/bharathkumarraju/Documents/work/2021/bharaths-google-credentials.json")
   project = "srianjaneyam"
 }
